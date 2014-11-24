8d382fad87749d09a25a00b3aef9add4fff99cfb
lutrasdebtra <stuy.bradley@gmail.com>
2014-11-25 09:28:22 +1300

    Rollback

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-BioinfRequests::Application.config.session_store :cookie_store, key: '_bioinf_requests_session', :expire_after => 2.hours, :domain => '.lt.local.com'
+BioinfRequests::Application.config.session_store :cookie_store, key: '_bioinf_requests_session', :expire_after => 2.hours
