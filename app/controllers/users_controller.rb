9530bbeb7dc46c55cfb2abc24edcb1ca02b0400e
lutrasdebtra <stuy.bradley@gmail.com>
2015-02-25 14:15:06 +1300

    Added customer requests to user pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @requests = Request.select {|x| x.name == current_user.login}
+    @requests = Request.select {|x| x.name == current_user.login || x.customer == current_user.login}
   end
 
   # GET /users/new
