77d9e16c48b3797fc4e1498386b5e9cbfa7852dd
lutrasdebtra <stuy.bradley@gmail.com>
2015-06-30 12:40:28 +1200

    Localised the mailer password.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
+load(app_env_vars)  if File.exists?(app_env_vars)
 
 # Initialize the Rails application.
 BioinfRequests::Application.initialize!
@@ -20,8 +22,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => "smtp.office365.com",
     :port => 587,
     :domain => "lanzatech.com",
-    :user_name => "SynBioAdmin@lanzatech.onmicrosoft.com",
-    :password => "$yn8i0LZ",
+    :user_name => ENV['EMAIL'],
+    :password => ENV['PASSWORD'],
     :authentication => :login, 
     :enable_starttls_auto => true
 }
