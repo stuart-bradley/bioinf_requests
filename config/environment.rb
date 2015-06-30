66ec21538a4a26c8a1f8fdc3d0fa75d54acb4173
lutrasdebtra <stuy.bradley@gmail.com>
2015-06-30 12:46:13 +1200

    Cleaned up environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -8,16 +8,6 @@ BioinfRequests::Application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 
-# ActionMailer::Base.smtp_settings = {
-#    :address => "smtp.gmail.com",
-#    :port => 587,
-#    :domain => "gmail.com",
-#    :user_name => "stuy.bradley@gmail.com",
-#    :password => "Br@dley1$",
-#    :authentication => "plain",
-#    :enable_starttls_auto => true
-# }
-
 ActionMailer::Base.smtp_settings = {
 	:address => "smtp.office365.com",
     :port => 587,
@@ -27,14 +17,3 @@ ActionMailer::Base.smtp_settings = {
     :authentication => :login, 
     :enable_starttls_auto => true
 }
-
-# ActionMailer::Base.smtp_settings = {
-# 	:address => "smtp.office365.com",
-#     :port => '587',
-#     :domain => "lanzatech.com",
-#     :user_name => "Stuart.Bradley@lanzatech.com",
-#     :password => "Hockey123",
-#     :authentication => :login, 
-#     :enable_starttls_auto => true
-# }
-
