97546325bf996528a93f41fa81a849123d0bed96
lutras debtra <stuy.bradley@gmail.com>
2017-02-28 12:09:06 +1300

    Added model tests for result files.

--- test/models/data_file_test.rb
+++ test/models/data_file_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class DataFileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "data files modification" do
+    request = Request.first
+    assert_difference('DataFile.count', -1) do
+      DataFile.save_data_files(nil, [DataFile.first.id], request)
+    end
+
+    assert_difference('DataFile.count') do
+      datafiles = {}
+      datafiles['attachment_uploader'] = ["Mystring"]
+      DataFile.save_data_files(datafiles, [], request)
+    end
+  end
 end
--- test/models/result_file_test.rb
+++ test/models/result_file_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ResultFileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "result files modification" do
+    request = Request.first
+    assert_difference('ResultFile.count', -1) do
+      ResultFile.save_result_files(nil, [ResultFile.first.id], request)
+    end
+
+    assert_difference('ResultFile.count') do
+      resultfiles = {}
+      resultfiles['attachment_uploader'] = ["Mystring"]
+      ResultFile.save_result_files(resultfiles, [], request)
+    end
+  end
 end
