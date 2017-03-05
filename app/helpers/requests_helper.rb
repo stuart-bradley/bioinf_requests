e64e5c56f8b3c480b9d6e5f0d42d824497926329
lutras debtra <stuy.bradley@gmail.com>
2017-03-05 21:00:12 +1300

    Added fonts to html5 editor via whitelisting of font tag and face attribute.

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,7 +1,7 @@
 module RequestsHelper
   def sanatize_wysihtml text
-    allowed_tags = (Loofah::HTML5::WhiteList::ALLOWED_ELEMENTS_WITH_LIBXML2.add('u'))
-    default_attributes = Loofah::HTML5::WhiteList::ALLOWED_ATTRIBUTES
+    allowed_tags = (Loofah::HTML5::WhiteList::ALLOWED_ELEMENTS_WITH_LIBXML2.add(%w['u','font']))
+    default_attributes = Loofah::HTML5::WhiteList::ALLOWED_ATTRIBUTES.add('face')
     utf8_text = text.to_s.force_encoding("UTF-8")
     sanitize utf8_text, tags: allowed_tags, attributes: default_attributes
   end
